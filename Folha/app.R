0146b032e8511eec89583767a782a45348258ada
Cássio Veludo <106350185+cassioveludo@users.noreply.github.com>
2023-03-09 13:10:22 -0300

    instruções

--- Folha/app.R
+++ Folha/app.R
@@ -1,3 +1,13 @@
+##############
+# Instruções #
+##############
+
+# 1) CLique no botão verde contendo o símbolo de play (>) com os dizeres "Run App"
+# 2) Uma janela abrirá. Você pode usar o App nesta janela ou
+# 3) Clique no botão "Open in Browser"; o App abrirá em uma janela do seu navegador
+
+
+
 library(shiny)
 
 shinyAppDir("src")
