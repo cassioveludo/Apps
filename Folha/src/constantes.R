24704dd1f64581964ec1cffe6cb3c12ad16ee23a
Cássio Veludo <106350185+cassioveludo@users.noreply.github.com>
2023-03-12 16:52:13 -0300

    test

--- Folha/src/constantes.R
+++
@@ -1,6 +0,0 @@
-APP_TITLE <- "Folha de Passagens VEMSE/DF"
-APP_VERSION <- "0.0.1"
-#data_last_day <- "2020-05-10" %>% as.Date()
-#data_first_day <- "2015-01-01" %>% as.Date()
-#marketplace_website <- "https://appsilon.com/"
-
